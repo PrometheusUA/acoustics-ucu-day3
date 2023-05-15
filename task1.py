b966dc00a2bb1224f22eb48da452076f7d23e7cb
prometheusua <PrometheusUA@i.ua>
2023-05-15 16:17:54 +0300

    task1 added torch version for loss

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 
 def levenstein(str_a, str_b) -> int:
@@ -30,6 +31,32 @@ def wer(str_a, str_b) -> float:
 
     len_ref = len(str_b_words)
     return levenstein_distance/len_ref
+
+    def levenstein_torch(str_a, str_b) -> int:
+        levenstein_matrix = torch.zeros((len(str_a) + 1, len(str_b) + 1), 
+                                        requires_grad=True)
+
+        levenstein_matrix[0, :] = torch.arange(len(str_b) + 1)
+        levenstein_matrix[:, 0] = torch.arange(len(str_a) + 1)
+
+        for i in range(len(str_a)):
+            for j in range(len(str_b)):
+                subst_cost = 1 if str_a[i] != str_b[j] else 0
+
+                levenstein_matrix[i + 1, j + 1] = torch.min(torch.tensor([
+                    levenstein_matrix[i, j + 1] + 1, 
+                    levenstein_matrix[i + 1, j] + 1, 
+                    levenstein_matrix[i, j] + subst_cost]))
+
+        return levenstein_matrix[-1, -1]
+
+    def wer_loss(pred: torch.Tensor, labels: torch.Tensor):
+        batch_size, sentence_size = pred.size()
+        losses = torch.FloatTensor((batch_size), requires_grad=True)
+        for i in range(batch_size):
+            distance = levenstein_torch(pred[i], labels[i])
+            losses[i] = distance / sentence_size
+        return torch.mean(losses)
 
 
 if __name__ == '__main__':
